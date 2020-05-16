2ebb2043bc1a15f49d4157b9089638884ac6db50
Kinga <-@DESKTOP-UAN2T14>
2020-05-16 15:19:55 +0300

    Initial commit of folders

---
+++ TaskAPI/Migrations/20200516111424_InitialCreate.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TaskAPI.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tasks",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    DateAdded = table.Column<DateTime>(nullable: false),
+                    DateDeadline = table.Column<DateTime>(nullable: false),
+                    Importance = table.Column<int>(nullable: false),
+                    Status = table.Column<int>(nullable: false),
+                    DateClosure = table.Column<DateTime>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tasks", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tasks");
+        }
+    }
+}
---
+++ TaskAPI/Models/SeedData.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskAPI.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new TaskContext(serviceProvider.GetRequiredService<DbContextOptions<TaskContext>>()))
+            {
+                // Look for any tasks.
+                if (context.Tasks.Any())
+                {
+                    return;   // DB table has been seeded
+                }
+
+                context.Tasks.AddRange(
+                    new TaskItem
+                    {
+                        Title = "Dentist",
+                        Description = "Baker Street 13, 12:00",
+                        DateAdded = DateTime.Now,
+                        DateDeadline = DateTime.Now,
+                        Importance = ImportanceList.high,
+                        Status = StatusList.open
+                    },
+
+                    new TaskItem
+                    {
+                        Title = "Presentation",
+                        Description = "Conference X, 13 april 12:00",
+                        DateAdded = DateTime.Now,
+                        DateDeadline = DateTime.Now,
+                        Importance = ImportanceList.high,
+                        Status = StatusList.in_progress
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
